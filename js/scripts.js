06f873c954a98f18786bcdd0e4188f7f2557fd06
sanghyuk07 <tkdgur933@gmail.com>
2021-01-11 00:15:13 +0900

    #2 js-interaction작업 및 스크롤 css추가 210111

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ const navbar =document.querySelector(".navbar");
 const logo =document.querySelector(".logo");
 const navbar__menu =document.querySelector(".navbar__menu");
 const menu__toggler =document.querySelector(".menu-toggler");
+const menu =document.querySelector(".menu")
+const menu_2 =document.querySelector(".menu_2")
+const menu_3 =document.querySelector(".menu_3")
+const menu_4 =document.querySelector(".menu_4")
+const menu_5 =document.querySelector(".menu_5")
+
 
 
 function Scrolled(){
@@ -16,10 +22,23 @@ function Scrolled(){
 function Display(){
     menu__toggler.classList.toggle('active');
     navbar__menu.classList.toggle('active');
+    
+  
+}
+function NonDisplay(){
+    navbar__menu.classList.remove('active');
+    menu__toggler.classList.toggle('active');
 }
 
-window.onscroll = function() {Scrolled()};
+function init(){
+    Scrolled()
+    window.onscroll = function() {Scrolled()};
+    menu__toggler.addEventListener("click", Display);
+    menu.addEventListener("click",NonDisplay);
+    menu_2.addEventListener("click",NonDisplay);
+    menu_3.addEventListener("click",NonDisplay);
+    menu_4.addEventListener("click",NonDisplay);
+    menu_5.addEventListener("click",NonDisplay);
 
-menu__toggler.addEventListener("click", Display);
-
-
+}
+init();
