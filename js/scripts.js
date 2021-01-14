9be8bc8cf4f9e7befb5e72e9a2d41c86b6842791
sanghyuk07 <tkdgur933@gmail.com>
2021-01-14 13:02:58 +0900

    #6 배경 사이즈 조절, 네이바 수정

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,63 @@
+const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo");
+const navbar__menu = document.querySelector(".navbar__menu");
+const menu__toggler = document.querySelector(".menu-toggler");
+const menu = document.querySelector(".menu");
+const menu_2 = document.querySelector(".menu_2");
+const menu_3 = document.querySelector(".menu_3");
+const menu_4 = document.querySelector(".menu_4");
+const menu_5 = document.querySelector(".menu_5");
 
-const navbar =document.querySelector(".navbar");
-const logo =document.querySelector(".logo");
-const navbar__menu =document.querySelector(".navbar__menu");
-const menu__toggler =document.querySelector(".menu-toggler");
-const menu =document.querySelector(".menu")
-const menu_2 =document.querySelector(".menu_2")
-const menu_3 =document.querySelector(".menu_3")
-const menu_4 =document.querySelector(".menu_4")
-const menu_5 =document.querySelector(".menu_5")
+const goTop = document.querySelector(".goTop");
 
-const goTop =document.querySelector(".goTop");
+window.onscroll = function sticky() {
+  if (window.pageYOffset > 20) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
 
 
-window.onscroll = function sticky(){
-    if(window.pageYOffset > 20){
-        navbar.classList.add("sticky");
-
-    }
-    else{
-        navbar.classList.remove("sticky");
-    }
-}
 // function Scrolled(){
 //     if(document.body.scrollTop > 20 || document.documentElement.scrollTop >20){
 //         navbar.classList.toggle('sticky');
-    
+
 //     }
 //     else{
 //         navbar.classList.remove('sticky');
 //     }
 // }
 
-function ScrollUp(){
-    scroll(0,0);
+function ScrollUp() {
+  scroll(0, 0);
 }
 
+function Display() {    
 
-function Display(){
-    menu__toggler.classList.toggle('active');
-    navbar__menu.classList.toggle('active');
-    
+  menu__toggler.classList.toggle("active");
+  navbar__menu.classList.toggle("active");
+  navbar.classList.remove("sticky");
+ 
+}
+function NonDisplay() {
+  navbar__menu.classList.remove("active");
+  menu__toggler.classList.toggle("active");
+ 
+
+  }
   
-}
-function NonDisplay(){
-    navbar__menu.classList.remove('active');
-    menu__toggler.classList.toggle('active');
-}
 
-function init(){
-    // Scrolled()
-    // window.onscroll = function() {sticky()};
-    menu__toggler.addEventListener("click", Display);
-    menu.addEventListener("click",NonDisplay);
-    menu_2.addEventListener("click",NonDisplay);
-    menu_3.addEventListener("click",NonDisplay);
-    menu_4.addEventListener("click",NonDisplay);
-    menu_5.addEventListener("click",NonDisplay);
-    goTop.addEventListener("click",ScrollUp);
 
+function init() {
+  // Scrolled()
+  // window.onscroll = function() {sticky()};
+  menu__toggler.addEventListener("click", Display);
+  menu.addEventListener("click", NonDisplay);
+  menu_2.addEventListener("click", NonDisplay);
+  menu_3.addEventListener("click", NonDisplay);
+  menu_4.addEventListener("click", NonDisplay);
+  menu_5.addEventListener("click", NonDisplay);
+  goTop.addEventListener("click", ScrollUp);
 }
 init();
